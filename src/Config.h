8afec3afe6d2b0ac5d736355628a61f9c2020eb1
Alexander Kurtz <alexkurtz@web.de>
2023-10-06 12:47:53 +0200

    Removed obsolete SEED variable

--- src/Config.h
+++ src/Config.h
@@ -6,7 +6,6 @@
 typedef float FloatPrecision;
 
 // Transposition Table
-extern const uint64_t SEED;
 extern std::vector<std::vector<int64_t>> zobrist_table;
 extern std::unordered_map<uint64_t, Statistics*>* TT;
 extern uint32_t TT_hits;
